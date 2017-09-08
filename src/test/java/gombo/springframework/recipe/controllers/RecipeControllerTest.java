b0fda0949b97823d0d84907025c81d67787ae53d
Charly Gomez <charly.gomez@gmx.at>
2017-09-08 10:40:57 +0200

    Updated RecipeControllerTest and Thymeleaf-templates

--- src/test/java/gombo/springframework/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/gombo/springframework/recipe/controllers/RecipeControllerTest.java
@@ -5,13 +5,16 @@ import gombo.springframework.recipe.service.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -40,9 +43,12 @@ public class RecipeControllerTest
 
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
 
+        Mockito.when(recipeService.getRecipeById(anyLong())).thenReturn(recipe);
+
         mockMvc.perform(get("/recipe/show/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
     }
 
 }
